85aa7e799cc296abb4b48e9ac9c6fcce19a3b8a9
Darkyyyyyyy7 <142657504+Darkyyyyyyy7@users.noreply.github.com>
2023-08-19 14:08:04 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    string fname,lname;
+    cin>>fname;
+
+    cout<<"Enter your first name: ";
+    cin>>fname;
+
+    cout<<"Enter your last name: ";
+    cin>>lname;
+
+    cout<<"Your full name is "<<fname<<" "<<lname;
+
+    return 0;
+}
